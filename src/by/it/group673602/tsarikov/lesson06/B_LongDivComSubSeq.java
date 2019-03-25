882a06ef7151051e23acbe8f6bf88020f1fff3ca
B0RlS <Dosya0318@gmail.com>
2019-03-25 21:36:05 +0300

    Add task B, lesson 06

--- src/by/it/group673602/tsarikov/lesson06/B_LongDivComSubSeq.java
+++ src/by/it/group673602/tsarikov/lesson06/B_LongDivComSubSeq.java
@@ -45,10 +45,59 @@ public class B_LongDivComSubSeq {
         //тут реализуйте логику задачи методами динамического программирования (!!!)
         int result = 0;
 
-
+        int[] d = new int[n];
+        for(int i = 0; i < n; i++){
+            d[i] = 1;
+            for(int j = 0; j < i; j++){
+                if(m[j] < m[i] && d[j] + 1 > d[i] && m[i]%m[j]==0){
+                    d[i] = d[i] + 1;
+                }
+            }
+        }
+        result = getMax(d);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
     }
+
+    private int getMax(int[] array){
+        quickSort(array, 0, array.length-1);
+        return array[array.length-1];
+    }
+
+    private void quickSort(int[] array, int low, int high){
+        if(array.length == 0){
+            return;
+        }
+        if(low >= high){
+            return;
+        }
+        int middle = low + (high-low)/2;
+        int opora = array[middle];
+        int i = low, j = high;
+        while (i <= j){
+            while (array[i] < opora){
+                i++;
+            }
+            while (array[j] > opora){
+                j--;
+            }
+            if(i<=j){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+            if(low < j){
+                quickSort(array, low, j);
+            }
+            if(high > i){
+                quickSort(array, i, high);
+            }
+        }
+    }
+
+
 
 
     public static void main(String[] args) throws FileNotFoundException {
