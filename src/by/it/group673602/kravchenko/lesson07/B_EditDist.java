a018a3ce3235a13b9405b32312c0634207a3b344
Kravchenko Andrey <andruha.kr90@gmail.com>
2019-04-02 10:36:17 +0300

    lesson06, lesson07 - A, B

---
+++ src/by/it/group673602/kravchenko/lesson07/B_EditDist.java
@@ -0,0 +1,98 @@
+package by.it.group673602.kravchenko.lesson07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Итерационно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class B_EditDist {
+
+    static int levenshtein(String one, String two) {
+        int[][] array = new int[one.length() + 1][two.length() + 1];
+
+        for (int i = 0; i <= one.length(); i++) {
+            for (int j = 0; j <= two.length(); j++) {
+                if (i == 0) {
+                    array[i][j] = j;
+                }
+                else if (j == 0) {
+                    array[i][j] = i;
+                }
+                else {
+                    array[i][j] = min(array[i - 1][j - 1]
+                                    + costOfSubstitution(one.charAt(i - 1), two.charAt(j - 1)),
+                            array[i - 1][j] + 1,
+                            array[i][j - 1] + 1);
+                }
+            }
+        }
+
+        return array[one.length()][two.length()];
+    }
+
+    public static int costOfSubstitution(char a, char b) {
+        return a == b ? 0 : 1;
+    }
+
+    public static int min(int... numbers) {
+        return Arrays.stream(numbers)
+                .min().orElse(Integer.MAX_VALUE);
+    }
+
+    int getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+
+        int result = 0;
+
+        result = levenshtein(one, two);
+
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson07/dataABC.txt");
+        B_EditDist instance = new B_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+    }
+
+}
