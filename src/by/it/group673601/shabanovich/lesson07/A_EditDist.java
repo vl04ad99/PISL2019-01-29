9de884390ef8e3f7e7c1abd3154dae236643e5cf
Lima <ms.shabanovich@mail.ru>
2019-04-02 10:45:42 +0300

    lesson06 AB, lesson07 AB

---
+++ src/by/it/group673601/shabanovich/lesson07/A_EditDist.java
@@ -0,0 +1,87 @@
+package by.it.group673601.shabanovich.lesson07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Рекурсивно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class A_EditDist {
+
+    private char[] oneChars;
+    private char[] twoChars;
+
+    int getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        oneChars = one.toCharArray();
+        twoChars = two.toCharArray();
+        return editDistTD(oneChars.length, twoChars.length);
+
+
+    }
+
+    private int editDistTD(int i, int j) {
+        if (i == 0 && j == 0) {
+            return 0;
+        } else {
+            if (i == 0) {
+                return j;
+            } else {
+                if (j == 0) {
+                    return i;
+                } else {
+                    if (oneChars[i - 1] == twoChars[j - 1]) {
+                        return editDistTD(i - 1, j - 1);
+                    } else {
+                        return Math.min(editDistTD(i - 1, j),
+                                Math.min(editDistTD(i, j - 1), editDistTD(i - 1, j - 1))) + 1;
+                    }
+                }
+            }
+        }
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/a_khmelev/lesson07/dataABC.txt");
+        A_EditDist instance = new A_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+    }
+}
+
