d9e265cf91793f1443ed9ca5c7b5fd8e38fee680
Osipenko Julia <julia.osipencko@mail.ru>
2019-04-16 12:19:30 +0300

    Lesson4,5 C Lesson6 A,B,C Lesson7 A,B

--- src/by/it/group673601/osipenko/lesson04/C_GetInversions.java
+++ src/by/it/group673601/osipenko/lesson04/C_GetInversions.java
@@ -3,6 +3,7 @@ package by.it.group673601.osipenko.lesson04;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -46,10 +47,9 @@ public class C_GetInversions {
         for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        int result = 0;
         //!!!!!!!!!!!!!!!!!!!!!!!!     тут ваше решение   !!!!!!!!!!!!!!!!!!!!!!!!
 
-
+        int result = inversionCount(a);
 
 
 
@@ -61,6 +61,36 @@ public class C_GetInversions {
         return result;
     }
 
+    private int inversionCount(int[] arr) {
+        if (arr.length < 2)
+            return 0;
+
+        int m = (arr.length + 1) / 2;
+        int left[] = Arrays.copyOfRange(arr, 0, m);
+        int right[] = Arrays.copyOfRange(arr, m, arr.length);
+
+        return inversionCount(left) + inversionCount(right) + merge(arr, left, right);
+    }
+    private int merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, count = 0;
+        while (i < left.length || j < right.length) {
+            if (i == left.length) {
+                arr[i+j] = right[j];
+                j++;
+            } else if (j == right.length) {
+                arr[i+j] = left[i];
+                i++;
+            } else if (left[i] <= right[j]) {
+                arr[i+j] = left[i];
+                i++;
+            } else {
+                arr[i+j] = right[j];
+                count += left.length-i;
+                j++;
+            }
+        }
+        return count;
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
